899a3c4a65163e21df8561ca34598612094b0afb
Dai Jiu <564343162@qq.com>
2023-04-12 23:52:29 +0800

    vsdfs

--- ExcelIO.Framework/CellProperty.cs
+++ ExcelIO.Framework/CellProperty.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Text;
 
 namespace ExcelIO.Framework
 {
@@ -14,7 +15,7 @@ namespace ExcelIO.Framework
 
         public TextAlign textAlign { get; set; }
 
-        public CellDataType cellDataType { get; set; } = CellDataType.Text1;
+        public CellDataType cellDataType { get; set; }
 
         private int GetIntFromHex(string strHex)
         {
